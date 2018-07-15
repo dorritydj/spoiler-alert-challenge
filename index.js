3e6c6fd6e84030273bba6152784f4a86f18eea7c
Daniel Dorrity <dorritydj@gmail.com>
2018-07-15 18:13:28 -0400

    Updating code for updated requirments

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ const costValues = {
     };
 
 function validateFoodUsage(food) {
-    //using javascript shortcircuit here because an if/switch statement would be too verbose,
-    //  and this makes things cleaner at the sacrifice of readability. 
     //The logic here boils down to checking the additional usages for the current category, 
     //  and if there are none, then check if the food usage is considered valid for all foods
     return (additionalUsagesMap[food.category] && additionalUsagesMap[food.category].includes(food.usage)) || allItemsUsage.includes(food.usage);
@@ -27,17 +25,25 @@ function calculateOverallUsageValue(listOfFood) {
     //loops through the supplied listOfFood items
     //  and checks to make sure the food has "valid" usage
     //  then adds to the total
-    listOfFood.forEach(food => {
-        if (validateFoodUsage(food)) {
-            totalValue += (food.cost * costValues[food.usage]);
-        }
-    });
+    if(listOfFood) {
+        listOfFood.forEach(food => {
+            //could use the shortcircuit here to save a line, but since it's not returning anything, it's probably
+            //  not best practice
+            if (validateFoodUsage(food)) {
+                totalValue += (food.cost * costValues[food.usage]);
+            }
+        });
+    }
 
-    //parsing value to be more inline with a dollar amount
-    /*
-        TODO: how to handle 1.555 (toFixed rounds to .55 and not .56)
-     */
-    return parseFloat(totalValue).toFixed(2);
+    //parseFloat to ensure we're changing a floating point number
+    //.toFixed has a weird issue due to how floating point numbers work in
+    //  JS. .555 won't round to .56, but .55 instead (not the case for .455 or .655).
+    //  Would probably consider an outside library in order to get the proper calculation
+    //I'm avoiding using simple arithmetic to do this because if, in the future, this needs to be
+    //  updated (from 2 decimal places to 3), this is much easier than trying to figure out a new 
+    //  formula. This also gives better justification to find an outside library to handle this 
+    //  for us. 
+    return new Number(parseFloat(totalValue).toFixed(2));
 }
 
 module.exports = calculateOverallUsageValue;
@@ -46,9 +52,11 @@ module.exports = calculateOverallUsageValue;
  * Additional comments/things to be considered for the future:
  *      1. costValue, allItemsUsage, and additionalUsagesMap should realistically 
  *          be coming from a DB or a config object, rather than hardcoded within the script itself.
- *      2. the use of an external utility library, such as lodash or underscore, could simplify
- *          and clean up a good portion of the code here (replacing .includes with .find, .concat 
- *          with more of an object merge), while being as optimized as possible. 
+ *          But the solution presented should be able to be used during a promise chain, observable subscribe,
+ *          or just importing the data directly. 
+ *      2. You could use an external utility library like lodash or underscore here, but the gains would be negligible.
+ *          If this was a fullscale application with the libraries already being used, then it would be worth using them,
+ *          but for a simple file like this, it would be overkill.
  *      3. I purposefully chose not to do any kind of data validation here (i.e., checking that
  *          all the categories are strings). In a real world scenario that's something that would
  *          (hopefully) be done in the data layer stage as things are attempted to be entered wherever 
--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -16,7 +16,7 @@ describe('calculateOverallUsageValue', () => {
                 {category: 'drinks',  cost: 2.00, usage: 'recycled'},
                 {category: 'meat',    cost: 5.00, usage: 'composted'},    
             ],
-            expectedResult: '1.40'
+            expectedResult: 1.40
         },
         //I'm focusing on testing requirements over things like "single item valid/invalid"
         //  or "all items valid/invalid". I feel as though those ideas are covered by 
@@ -31,18 +31,34 @@ describe('calculateOverallUsageValue', () => {
                 {category: 'drinks', cost: 1.00, usage: 'recycled'},
                 {category: 'meat', cost: 1.00, usage: 'recycled'}
             ],
-            expectedResult: '0.25'
+            expectedResult: 0.25
         },
         {
             //The idea here was to use different categories than those of the additionally defined
             //  requirements. Because trashed and eaten can be used for any food category,
-            //  testing them through different categories, will also test them through the ones above
+            //  testing them through different categories, will also test them through the ones above.
+            //As well as adding a case that is known not to work to ensure that doesn't contribute to the calculation
             testName: 'base usage requirements',
             data: [
                 {category: 'meat', cost: 1.00, usage: 'trashed'},
                 {category: 'snacks', cost: 1.00, usage: 'eaten'},
+                {category: 'snacks', cost: 1.00, usage: 'composted'}
             ],
-            expectedResult: '1.00'
+            expectedResult: 1.00
+        }, 
+        {
+            testName: 'when there is no data',
+            data: [],
+            expectedResult: 0.00
+        },
+            //This is more of a "worse case scenario" test, where something bad happened along the process
+            //  and for some reason, the data we're sending to the function is just empty and doesn't exist.
+            //The function should still handle this gracefully since there's no explicit requirements for 
+            //  error validation
+        {
+            testName: 'when the data is null',
+            data: null,
+            expectedResult: 0.00
         }
     ]
     .forEach((config, index) => {
